5a9830a34370727334a23098f2a44857aa5d2471
elydon <markus@kuehne-welt.de>
2015-12-08 20:13:25 +0100

    intial commit of fragments-core and master project

---
+++ fragments-core/src/main/java/de/elydon/fragments/core/Fragment.java
@@ -0,0 +1,181 @@
+package de.elydon.fragments.core;
+
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+/**
+ * <p>
+ * A fragment is a single piece of knowledge.
+ * </p>
+ * <p>
+ * It holds a header with up to 100 characters and a text with at most 2000
+ * characters, and optionally a source URL, and an image.
+ * </p>
+ * 
+ * @author elydon
+ *
+ */
+public class Fragment {
+
+	private long id;
+
+	private String header;
+
+	private String text;
+
+	private BufferedImage image;
+
+	private URL source;
+
+	/**
+	 * <p>
+	 * Constructs a {@link Fragment} with the specified header and text.
+	 * </p>
+	 * 
+	 * @param header
+	 * @param text
+	 * @throws IllegalArgumentException
+	 *             If any of the arguments is {@code null} or the header exceeds
+	 *             100 characters or the text exceeds 2000 characters
+	 */
+	public Fragment(final String header, final String text) {
+		setHeader(header);
+		setText(text);
+	}
+
+	/**
+	 * <p>
+	 * Sets the fragment's unique identifier.
+	 * </p>
+	 * <p>
+	 * The caller is responsible to guarantee that the ID is indeed unique.
+	 * </p>
+	 * <p>
+	 * The ID has to be greater than zero, as negative IDs are not allowed and a
+	 * value of zero indicates, that the fragment has not been
+	 * {@link FragmentManager#store(Fragment) stored} yet.
+	 * </p>
+	 * 
+	 * @param id
+	 */
+	public void setId(long id) {
+		if (id < 1) {
+			throw new IllegalArgumentException("ID must be at least 1");
+		}
+		this.id = id;
+	}
+
+	/**
+	 * <p>
+	 * Gets the unique identifier of the fragment.
+	 * </p>
+	 * <p>
+	 * If zero, the fragment has not been {@link FragmentManager#store(Fragment)
+	 * stored} yet.
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * <p>
+	 * Set the text of the fragment.
+	 * </p>
+	 * 
+	 * @param text
+	 * @throws IllegalArgumentException
+	 *             If the text is {@code null} or exceeds 2000 characters
+	 */
+	public void setText(String text) {
+		if (text == null || text.length() > 2000) {
+			throw new IllegalArgumentException("Text must not be null and contain max. 2000 characters");
+		}
+		this.text = text;
+	}
+
+	/**
+	 * <p>
+	 * Returns the fragment's text.
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * <p>
+	 * Returns the fragment's header.
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public String getHeader() {
+		return header;
+	}
+
+	/**
+	 * <p>
+	 * Set the header of the fragment.
+	 * </p>
+	 * 
+	 * @param header
+	 * @throws IllegalArgumentException
+	 *             If the header is {@code null} or exceeds 100 characters
+	 */
+	public void setHeader(String header) {
+		if (header == null || header.length() > 100) {
+			throw new IllegalArgumentException("Header must not be null and contain max. 100 characters");
+		}
+		this.header = header;
+	}
+
+	/**
+	 * <p>
+	 * Gets the fragment's source URL.
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public URL getSource() {
+		return source;
+	}
+
+	/**
+	 * <p>
+	 * Set the fragment's source URL.
+	 * </p>
+	 * 
+	 * @param source
+	 */
+	public void setSource(URL source) {
+		this.source = source;
+	}
+
+	/**
+	 * <p>
+	 * Returns the fragment's image.
+	 * </p>
+	 * 
+	 * @return
+	 */
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	/**
+	 * <p>
+	 * Sets the fragment's image.
+	 * </p>
+	 * 
+	 * @param image
+	 */
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+}
---
+++ fragments-core/src/main/java/de/elydon/fragments/core/FragmentManager.java
@@ -0,0 +1,93 @@
+package de.elydon.fragments.core;
+
+import java.util.List;
+
+/**
+ * <p>
+ * Manages the {@link Fragment fragments} in the system.
+ * </p>
+ * 
+ * @author elydon
+ *
+ */
+public interface FragmentManager {
+
+	/**
+	 * <p>
+	 * Returns the {@link Fragment} with the specified id or {@code null}, if no
+	 * such fragment exists.
+	 * </p>
+	 * 
+	 * @param id
+	 * @return
+	 */
+	Fragment get(int id);
+
+	/**
+	 * <p>
+	 * Returns a (probably unsorted) list of all {@link Fragment fragments} in
+	 * the system.
+	 * </p>
+	 * 
+	 * @return
+	 */
+	List<Fragment> getAll();
+
+	/**
+	 * <p>
+	 * Stores the {@link Fragment} in the system. If it has not been stored yet,
+	 * this method {@link Fragment#setId(long) sets a new ID}.
+	 * </p>
+	 * 
+	 * @param fragment
+	 * @return The stored fragment
+	 * @throws IllegalArgumentException
+	 *             If the fragment is {@code null}
+	 */
+	Fragment store(Fragment fragment);
+
+	/**
+	 * <p>
+	 * Deletes the fragment with the specified ID.
+	 * </p>
+	 * 
+	 * @param id
+	 * @return The deleted fragment
+	 * @throws IllegalArgumentException
+	 *             If no fragment with the specified ID exists
+	 */
+	Fragment delete(int id);
+
+	/**
+	 * <p>
+	 * Searches for {@link Fragment fragments} that match the search query.
+	 * </p>
+	 * <p>
+	 * The syntax of the query as well as the search algorithm are
+	 * implementation specific.
+	 * </p>
+	 * 
+	 * @param query
+	 * @return The found list of fragments, or the empty list if none were found
+	 * @throws IllegalArgumentException
+	 *             If the query is {@code null}
+	 */
+	List<Fragment> search(String query);
+
+	/**
+	 * <p>
+	 * Fetches a list of related fragments to the specified one.
+	 * </p>
+	 * <p>
+	 * The algorithm of determining which fragments are related is
+	 * implementation specific.
+	 * </p>
+	 * 
+	 * @param fragment
+	 * @return A list of related fragments, or the empty list if none are found
+	 * @throws IllegalArgumentException
+	 *             If the fragment is {@code null}
+	 */
+	List<Fragment> fetchRelated(Fragment fragment);
+
+}
