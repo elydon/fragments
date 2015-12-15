6cbf88cdfac354ac146787c361e2dca037e0428c
elydon <markus@kuehne-welt.de>
2015-12-15 19:52:34 +0100

    ClassScanner is now capable of examining JAR files

--- fragments-core/src/main/java/de/elydon/fragments/core/ClassScanner.java
+++ fragments-core/src/main/java/de/elydon/fragments/core/ClassScanner.java
@@ -1,6 +1,7 @@
 package de.elydon.fragments.core;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -9,6 +10,8 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ClassScanner {
 
@@ -25,15 +28,36 @@ public class ClassScanner {
 				public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
 					if (file.toString().endsWith(".class")) {
 						System.out.println("  scanning file " + file);
-						final String classname = file.toString().replace(root.toString() + File.separator, "").replace(File.separator, ".").replace(".class", "");
+						String classname = file.toString().replace(root.toString() + File.separator, "").replace(File.separator, ".");
+						classname = classname.substring(0, classname.length() - ".class".length());
 						try {
 							final Class<?> clazz = classLoader.loadClass(classname);
 							if (filter.accepts(clazz)) {
-								System.out.println("added " + classname);
+								System.out.println("    added " + classname);
 								classes.add(clazz);
 							}
 						} catch (final ClassNotFoundException e) {
 							System.err.println(classname + " could not be loaded from resource " + file);
+						}
+					} else if (file.toString().endsWith(".jar")) {
+						System.out.println("  scanning JAR file " + file);
+						try (final ZipInputStream zip = new ZipInputStream(new FileInputStream(file.toFile()))) {
+							for (ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
+							    if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
+							    	// extract class name
+							        String classname = entry.getName().replace('/', '.');
+							        classname = classname.substring(0, classname.length() - ".class".length());
+							        try {
+										final Class<?> clazz = classLoader.loadClass(classname);
+										if (filter.accepts(clazz)) {
+											System.out.println("    added " + classname);
+											classes.add(clazz);
+										}
+									} catch (final ClassNotFoundException e) {
+										System.err.println(classname + " could not be loaded from resource " + file);
+									}
+							    }
+							}
 						}
 					}
 					return FileVisitResult.CONTINUE;
